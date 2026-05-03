54c67a6b392d8b9aec189f6d403774b40f54816a
Rxmosx <lazaro.scolari@gmail.com>
2026-05-03 19:06:20 -0300

    fix: try

--- back/mysite/settings.py
+++ back/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -98,7 +99,7 @@ DATABASES = {
         'NAME': 'gestor_db',
         'USER': 'postgres',
         'PASSWORD': '1234',
-        'HOST': 'db',
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
         'PORT': '5432',
     }
 }
